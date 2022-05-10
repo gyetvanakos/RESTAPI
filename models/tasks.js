85b8e8a9871565e594281005327b817fdecfdc8c
gyetvanakos <gyetvanakos@gmail.com>
2022-05-10 13:36:36 +0200

    Update tasks.js

--- models/tasks.js
+++ models/tasks.js
@@ -1,12 +1,31 @@
 const mongoose = require ("mongoose");
 const Schema = mongoose.Schema;
 const User = require('../models/users');
+const statuses = [{
+    status: "open",
+    icon: "⭕️",
+    color: "#EB5A46"
+}, {
+    status: "in progress",
+    icon: "🔆️",
+    color: "#00C2E0"
+}, {
+    status: "in review",
+    icon: "📝",
+    color: "#C377E0"
+}, {
+    status: "done",
+    icon: "✅",
+    color: "#3981DE"
+}];
 
 let taksSchema = new Schema(
     {
         task: { type: String, required: true, minlength: 4, maxlength: 50 },
         taskDescription: { type: String, required: true },
-        status: { type: String },
+        status: {
+            type: String, statuses, default:"open"
+        },
         project: { type: Schema.Types.ObjectId, required: true, ref:'projects'},
         owner: { type: Schema.Types.ObjectId, required: true, ref:'users'},
         date: { type: Date, default: Date.now },
