146dc2aa9ddd9be0cfeb3368fc3922b1069f3be1
gyetvanakos <gyetvanakos@gmail.com>
2022-05-19 15:44:48 +0200

    Update project.js

--- routes/project.js
+++ routes/project.js
@@ -1,61 +1,80 @@
-const router = require ("express").Router();
-const projects = require ("../models/projects");
+const router = require("express").Router();
+const projects = require("../models/projects");
 const NodeCache = require('node-cache');
-const cache = new NodeCache({stdTTL: 600});
-const { verifyToken } = require("../validation");
+const cache = new NodeCache({
+    stdTTL: 600
+});
+const {
+    verifyToken
+} = require("../validation");
 
 router.post("/", verifyToken, (req, res) => {
-    
+
     data = req.body;
 
     projects.create(data)
-    .then(data => {
-        res.send(data);})
-    .catch(err => {res.status(500).send({message: err.message });})
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message
+            });
+        })
 });
 
 
 router.get("/:userId/", verifyToken, async (req, res) => {
     const userId = req.params.userId;
 
-        try{
-            let data = await projects.find().where('ownerId').equals(userId);
-            res.send(data)
-        }
-        catch(err){
-            res.status(500).send({message: err.message})
-        }
-    });
+    try {
+        let ownedProjects = await projects.find().where('ownerId').equals(userId);
+        let invitedProject = await projects.find().where('users').in([userId])
 
-    router.get("/:id", verifyToken, async (req, res) => {
-        const id = req.params.id
-        
-            try{
-                let data = await projects.findById(id);
-                res.send(data)
-            }
-            catch(err){
-                res.status(500).send({message: err.message})
-            }
-        });   
+        let data = ownedProjects.concat(invitedProject);
+        res.send(data)
+    } catch (err) {
+        res.status(500).send({
+            message: err.message
+        })
+    }
+});
 
-    
+router.get("/:id", verifyToken, async (req, res) => {
+    const id = req.params.id
+
+    try {
+        let data = await projects.findById(id);
+        res.send(data)
+    } catch (err) {
+        res.status(500).send({
+            message: err.message
+        })
+    }
+});
+
+
 router.put("/:id", verifyToken, (req, res) => {
 
     const id = req.params.id;
 
     projects.findByIdAndUpdate(id, req.body)
-    .then(data => { 
-        if(!data)
-        {
-            res.status(404).send({message: "Cannot update project :( id=" + id})
-        }
-        else
-        {
-            res.send({ message: "project is updated :)"})
-        }
-    })
-    .catch(err => { res.status(500).send({ message: "error updating product with id=" + id }); });
+        .then(data => {
+            if (!data) {
+                res.status(404).send({
+                    message: "Cannot update project :( id=" + id
+                })
+            } else {
+                res.send({
+                    message: "project is updated :)"
+                })
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "error updating product with id=" + id
+            });
+        });
 
 });
 
@@ -64,17 +83,22 @@ router.delete("/:id", verifyToken, (req, res) => {
     const id = req.params.id;
 
     projects.findByIdAndDelete(id)
-    .then(data => { 
-        if(!data)
-        {
-            res.status(404).send({message: "Cannot delete project :( id=" + id})
-        }
-        else
-        {
-            res.send({ message: "Project is deleted :)"})
-        }
-    })
-    .catch(err => { res.status(500).send({ message: "error deleting product with id=" + id }); });
+        .then(data => {
+            if (!data) {
+                res.status(404).send({
+                    message: "Cannot delete project :( id=" + id
+                })
+            } else {
+                res.send({
+                    message: "Project is deleted :)"
+                })
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "error deleting product with id=" + id
+            });
+        });
 
 });
 
